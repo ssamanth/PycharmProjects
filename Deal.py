c77fa34bb2dd5952bfbcee3fb6bce1a4318eba2d
Samantha Snyder <Samantha Snyder>
2015-11-05 10:29:18 -0500

    Initial commit

---
+++ Deal.py
@@ -0,0 +1,60 @@
+import random
+
+SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"]
+RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8",
+         "9", "10", "Jack", "Queen", "King"]
+CARDS = 52
+CARDS_PER_HAND = 13
+PLAYER = 0
+
+def main():
+    North = []
+    East = []
+    South = []
+    West = []
+
+
+    # Create a deck as a list of integers from 0 to 51
+    deck = list(range(CARDS))
+    # Shuffle the cards
+    random.shuffle(deck)
+    # Shuffle again, because once is never enough!
+    random.shuffle(deck)
+
+    # Pop a cart from the top of the deck, deal to each player, increment until player has 13 cards.
+    print("North was dealt")
+    print("---------------")
+    i = 0
+    while i < CARDS_PER_HAND:
+        card = deck.pop()
+        North.append(card)
+        North.sort()
+        print(North)
+        i += 1
+    print()
+    print("East was dealt:")
+    print("---------------")
+    i = 0
+    while i < CARDS_PER_HAND:
+        card = deck.pop()
+        print(RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
+        i += 1
+    print()
+    print("South was dealt:")
+    print("---------------")
+    i = 0
+    while i < CARDS_PER_HAND:
+        card = deck.pop()
+        print(RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
+        i += 1
+    print()
+    print("West was dealt:")
+    print("---------------")
+    i = 0
+    while i < CARDS_PER_HAND:
+        card = deck.pop()
+        print(RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
+        i += 1
+
+## can be returned from the deal function
+main()
