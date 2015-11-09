1af1d1ecc6e16a47c73966dc435829f029a32493
Samantha <ssamanth@dtcc.edu>
2015-11-09 10:32:27 -0500

    done bro

--- Deal.py
+++ Deal.py
@@ -5,14 +5,14 @@ RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8",
          "9", "10", "Jack", "Queen", "King"]
 CARDS = 52
 CARDS_PER_HAND = 13
-PLAYER = 0
+
 
 def main():
+    # Create empty sets to be filled
     North = []
     East = []
     South = []
     West = []
-
 
     # Create a deck as a list of integers from 0 to 51
     deck = list(range(CARDS))
@@ -21,50 +21,57 @@ def main():
     # Shuffle again, because once is never enough!
     random.shuffle(deck)
 
-    # Pop a cart from the top of the deck, deal to each player, increment until player has 13 cards.
-
+    # Pop a card from the top of the deck, deal to each player, increment until player has 13 cards.
+    # reorder the populated list, translate the int value to strings and print, respectively.
+    print("North was dealt:")
+    print("----------------")
     i = 0
     while i < CARDS_PER_HAND:
         card = deck.pop()
         North.append(card)
         North.sort()
         i += 1
-        # print(North)
-    for i in range(len(North)):
-        i = [RANKS[deck.pop(1) % CARDS_PER_HAND], 'of', SUITS[deck.pop(1) // CARDS_PER_HAND], ]
-        print(i)
-        #print("\n".join(map(str, North)))
+    for i in North:
+        print(RANKS[i % CARDS_PER_HAND], 'of', SUITS[i // CARDS_PER_HAND])
+    print()
+    print()
 
-    #
-    #
-    #
-    #
-    #
-    #
     print("East was dealt:")
     print("---------------")
     i = 0
     while i < CARDS_PER_HAND:
         card = deck.pop()
-        print(RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
+        East.append(card)
+        East.sort()
         i += 1
+    for i in East:
+        print(RANKS[i % CARDS_PER_HAND], 'of', SUITS[i // CARDS_PER_HAND])
     print()
+    print()
+
     print("South was dealt:")
     print("---------------")
     i = 0
     while i < CARDS_PER_HAND:
-        card = deck.pop()
-        print(RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
-        i += 1
+       card = deck.pop()
+       South.append(card)
+       South.sort()
+       i += 1
+    for i in South:
+       print(RANKS[i % CARDS_PER_HAND], 'of', SUITS[i // CARDS_PER_HAND])
     print()
+    print()
+
     print("West was dealt:")
     print("---------------")
     i = 0
     while i < CARDS_PER_HAND:
         card = deck.pop()
-        print(RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
+        West.append(card)
+        West.sort()
         i += 1
+    for i in West:
+        print(RANKS[i % CARDS_PER_HAND], 'of', SUITS[i // CARDS_PER_HAND])
 
 
-## can be returned from the deal function
 main()
